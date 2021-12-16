0d8d2ebdee25b4cb32cf023671dc8bff0ee5f8f7
Dave Serke <39059316+dopplerdaver@users.noreply.github.com>
2021-12-16 10:16:07 -0700

    Add files via upload
    update commenting

--- IFI_compare_RadIA_PIREP.py
+++ IFI_compare_RadIA_PIREP.py
@@ -44,15 +44,17 @@ Rv3PIR_FZDZ        = pd.read_csv(Rv3PIR_dir+Rv3PIR_FZDZ_name, header=0, index_co
 Rv3PIR_SSLW        = pd.read_csv(Rv3PIR_dir+Rv3PIR_SSLW_name, header=0, index_col=0)
 Rv3PIR_PIRP        = pd.read_csv(Rv3PIR_dir+Rv3PIR_PIRP_name, header=0, index_col=0)
 
+# ... low res countries dataset
 countries          = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
 
 #-------------------------------------------
 # MANIPULATE INPUT DATA
 #-------------------------------------------
-# ... concatenate input pandas dfs
+# ... concatenate Rv3 and PIRP input pandas dfs into one df
 Rv3PIR_ALL         = pd.concat([Rv3PIR_FZDZ, Rv3PIR_SSLW, Rv3PIR_PIRP], axis=1)
 #Rv3PIR_MAXint      = Rv3PIR_ALL[[' fzdz_interestmax', ' slw_interestmax']]
 
+# ... indexing/filtering of dataframe values
 PIRP_tot_num       = np.array(Rv3PIR_ALL[' iint1'])[np.array(Rv3PIR_ALL[' iint1'])].shape[0]
 PIRP_pos_num       = np.array(Rv3PIR_ALL[' iint1'])[np.array(Rv3PIR_ALL[' iint1']) > 0.0].shape[0]
 PIRP_neg_num       = np.array(Rv3PIR_ALL[' iint1'])[np.array(Rv3PIR_ALL[' iint1']) < 0.0].shape[0]
@@ -65,28 +67,47 @@ FZDZ_neg_num       = (np.array(Rv3PIR_ALL[' iint1'])[(Rv3PIR_ALL[' fzdz_interest
 FZDZ_neg           = [np.array(Rv3PIR_ALL[' iint1'])[(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float) < 0.5]]
 
 Rv3_pos_ind        = [(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float) >= 0.5] or [(Rv3PIR_ALL[' slw_interestmax']).astype(np.float) >= 0.5]
-Rv3_neg_ind        = [(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float) < 0.5] or [(Rv3PIR_ALL[' slw_interestmax']).astype(np.float) < 0.5]
-print(sum(sum(Rv3_pos_ind)))
-print(sum(sum(Rv3_neg_ind)))
+Rv3_neg_ind        = [(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float) < 0.5] and [(Rv3PIR_ALL[' slw_interestmax']).astype(np.float) < 0.5]
+Rv3_pos_num        = sum(sum(Rv3_pos_ind))
+Rv3_neg_num        = sum(sum(Rv3_neg_ind))
+
+FZDZ_nan_bool      = (Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float).isna()
+SLW_nan_bool       = (Rv3PIR_ALL[' slw_interestmax']).astype(np.float).isna()
 
 #-------------------------------------------
 # PLOTS
 #-------------------------------------------
-cMap = 'viridis'
-# ... scatter of R-v3 FZDZ/SSLW ints with PIREP sev color-coded points
+#def mscatter(x,y,ax=None, m=None, **kw):
+#    import matplotlib.markers as mmarkers
+#    if not ax: ax=plt.gca()
+#    sc = ax.scatter(x,y,**kw)
+#    if (m is not None) and (len(m)==len(x)):
+#        paths = []
+#        for marker in m:
+#            if isinstance(marker, mmarkers.MarkerStyle):
+#                marker_obj = marker
+#            else:
+#                marker_obj = mmarkers.MarkerStyle(marker)
+#            path = marker_obj.get_path().transformed(
+#                        marker_obj.get_transform())
+#            paths.append(path)
+#        sc.set_paths(paths)
+#    return sc
+
+cMap    = 'viridis'
 fig, ax = plt.subplots(figsize = (15, 15))
-#for i, ind in enumerate(np.array(Rv3PIR_ALL[' iint1']).astype(np.float)):
-#    if np.array(Rv3PIR_ALL[' iint1']).astype(np.float) >= 1:
+m       = ['*','o','o','o','o','o','o','o','o','o']
+#scatter = mscatter(np.array(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float), np.array(Rv3PIR_ALL[' slw_interestmax']).astype(np.float), c=np.array(Rv3PIR_ALL[' iint1']).astype(np.float), s=75, m=m, ax=ax)
+#plt.show()
+# ... scatter plot of R-v3 FZDZ/SSLW ints with PIREP sev color-coded points
 ax.scatter(np.array(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float), np.array(Rv3PIR_ALL[' slw_interestmax']).astype(np.float), c=np.array(Rv3PIR_ALL[' iint1']).astype(np.float), cmap=cMap, vmin=-1, vmax=8, s=75, marker='o')
-#    else:
-#       ax.scatter(np.array(Rv3PIR_ALL[' fzdz_interestmax'][i]).astype(np.float), np.array(Rv3PIR_ALL[' slw_interestmax'][i]).astype(np.float), c=np.array(Rv3PIR_ALL[' iint1'][i]).astype(np.float), s=75, marker='x')
 ax.grid(color='grey', linestyle='--', linewidth=1)
 ax.set_title('RadIA-v3 INT(MAX) near PIREPs for ICICLE F17', fontsize=20)
-ax.text(0.75, 0.60, 'N(P-TOT) = '+str(PIRP_tot_num), fontsize = 20)
-ax.text(0.75, 0.55, 'N(P-POS) = '+str(PIRP_pos_num), fontsize = 20)
-ax.text(0.80, 0.50, 'N(R-G50) = '+str(sum(sum(Rv3_pos_ind))), fontsize = 20)
-ax.text(0.75, 0.45, 'N(P-NEG) = '+str(PIRP_neg_num), fontsize = 20)
-ax.text(0.80, 0.40, 'N(R-L50) = '+str(sum(sum(Rv3_neg_ind))), fontsize = 20)
+ax.text(0.02, 0.61, 'N(P-TOT) = '+str(PIRP_tot_num),          c='black', fontsize = 20)
+ax.text(0.02, 0.56, 'N(P-POS) = '+str(PIRP_pos_num),          c='green', fontsize = 20)
+ax.text(0.07, 0.51, 'N(R-G50) = '+str(sum(sum(Rv3_pos_ind))), c='green', fontsize = 20)
+ax.text(0.02, 0.46, 'N(P-NEG) = '+str(PIRP_neg_num),          c='red',   fontsize = 20)
+ax.text(0.07, 0.41, 'N(R-L50) = '+str(sum(sum(Rv3_neg_ind))), c='red',   fontsize = 20)
 ax.set_xlabel('FZDZ INT', fontsize = 16)
 ax.set_ylabel('SSLW INT', fontsize = 16)
 ax.plot([0.0, 0.5], [0.5, 0.5], 'r--', label='test')
@@ -98,13 +119,21 @@ plt.ylim(0.0, 1.02)
 #plt.colorbar(cax=cax)
 plt.show()
 
+# ... scatter plot of R-v3 FZDZ ints versus PIREP reporting height with PIREP sev color-coded points
+fig, ax = plt.subplots(figsize = (15, 15))
+ax.scatter(np.array(Rv3PIR_ALL[' fzdz_interestmax']).astype(np.float), Rv3PIR_ALL[' flvl'], c=np.array(Rv3PIR_ALL[' iint1']).astype(np.float), cmap=cMap, vmin=-1, vmax=8, s=75, marker='o')
+ax.set_xlabel('FZDZ INT', fontsize = 16)
+ax.set_ylabel('F-lvl [kft]', fontsize = 16)
+plt.grid(b=True, alpha=0.5)
+plt.show()
+
 # ... mapview plot of PIREP locations
 # ......initialize an axis
 fig, ax = plt.subplots(figsize=(25,18))
 # ......plot map on axis
 countries[countries["name"] == "United States of America"].plot(color="lightgrey", ax=ax)
 # ......plot points
-Rv3PIR_ALL.plot(x=" lon", y=" lat", kind="scatter", 
+Rv3PIR_ALL.plot(x=" lon", y=" lat", kind="scatter", s=75, marker='o', 
                 c=' iint1', colormap=cMap, vmin=-1, vmax=8, 
                 title='PIREP location/severity in CONUS during 2/17/2019', 
                 ax=ax)
